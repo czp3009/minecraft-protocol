955fa2cbddf638d000c12662fcf04d988b2895e0
czp3009 <czp3009@gmail.com>
2026-08-06 12:18:04 +0800

    reduce: lower `MAX_PARALLEL_USAGES` in `MinecraftTestFixtureLimits` to 4

--- buildSrc/src/main/kotlin/com/hiczp/minecraft/buildlogic/MinecraftTestFixtureLimits.kt
+++ buildSrc/src/main/kotlin/com/hiczp/minecraft/buildlogic/MinecraftTestFixtureLimits.kt
@@ -2,5 +2,5 @@ package com.hiczp.minecraft.buildlogic
 
 /** Shared concurrency capacity for Gradle fixture tasks and Host resources. */
 object MinecraftTestFixtureLimits {
-    const val MAX_PARALLEL_USAGES: Int = 8
+    const val MAX_PARALLEL_USAGES: Int = 4
 }
