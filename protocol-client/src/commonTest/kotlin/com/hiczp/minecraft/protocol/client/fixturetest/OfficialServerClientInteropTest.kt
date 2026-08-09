c692bbef34afbe6417cced43ddff962336333548
czp3009 <czp3009@gmail.com>
2026-08-10 01:03:15 +0800

    test: add timeout to interop test for improved reliability

--- protocol-client/src/commonTest/kotlin/com/hiczp/minecraft/protocol/client/fixturetest/OfficialServerClientInteropTest.kt
+++ protocol-client/src/commonTest/kotlin/com/hiczp/minecraft/protocol/client/fixturetest/OfficialServerClientInteropTest.kt
@@ -3,10 +3,13 @@ package com.hiczp.minecraft.protocol.client.fixturetest
 import com.hiczp.minecraft.protocol.client.OfficialServerClientInteropRunner
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.time.Duration.Companion.minutes
 
 class OfficialServerClientInteropTest {
     @Test
-    fun productionClientReachesPlayAgainstOfficialServer() = runTest {
+    fun productionClientReachesPlayAgainstOfficialServer() = runTest(
+        timeout = 2.minutes
+    ) {
         OfficialServerClientInteropRunner.run()
     }
 }
